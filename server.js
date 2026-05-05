706845420d3e331f830da20e0f36dac3d31cc6a0
Chetana-R-Mathapati <chetanamathapati594@gmail.com>
2026-05-05 11:47:48 +0530

    fix cors and socket.io

--- server.js
+++ server.js
@@ -8,16 +8,13 @@ const fs = require("fs");
 
 const app = express();
 
-app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-  next();
-});
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
+}));
 
+app.use(express.json());
 require("dotenv").config();
 
 
@@ -29,7 +26,6 @@ const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
-  transports: ["websocket"],
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
